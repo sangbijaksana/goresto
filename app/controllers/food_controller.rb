6fecd652cff5a8f5bf5e83268666ba8e2f150cd1
Azis Adi Kuncoro <azis.adi@bukalapak.com>
2020-03-15 10:12:56 +0700

    add test for post controller

--- app/controllers/food_controller.rb
+++ app/controllers/food_controller.rb
@@ -1,8 +1,28 @@
 class FoodController < ApplicationController
+  def index
+    @foods = Food.all
+  end
+
   def new
   end
 
   def create
-    render plain: params[:food].inspect
+    @food = Food.new(food_params)
+ 
+    if @food.save
+      redirect_to @food
+    else
+      render 'new'
+    end
   end
+
+  def show
+    @food = Food.find(params[:id])
+  end
+
+  private
+    def food_params
+      params.require(:food).permit(:name, :description, :price)
+    end
 end
+
--- spec/controllers/food_controller_spec.rb
+++ spec/controllers/food_controller_spec.rb
@@ -2,15 +2,27 @@ require 'rails_helper'
 
 RSpec.describe FoodController, type: :controller do
 
-  describe "GET #new" do
+  describe 'GET #new' do
     subject { get :new }
 
-    it "renders the new template" do
+    it 'renders the new template' do
       expect(subject).to render_template(:new)
-      expect(subject).to render_template("new")
-      expect(subject).to render_template("food/new")
+      expect(subject).to render_template('new')
+      expect(subject).to render_template('food/new')
     end
-
   end
 
+  describe 'POST #create' do
+
+    it 'renders the show template' do
+      post :create, :params => {
+        :food => { 
+          :name => 'nasi',
+          :description => 'makanan pokok',
+          :price => 3000
+        }
+      }
+      expect(response).to redirect_to(assigns(:food))
+    end
+  end
 end
